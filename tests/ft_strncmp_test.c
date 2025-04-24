159bdb9bde91b7bf4090e65dc715f063f6a11e7a
Anna Kolupaeva <akolupae@c1r3p6.hive.fi>
2025-04-24 20:51:51 +0300

    Tested ft_strncmp

--- tests/ft_strncmp_test.c
+++ tests/ft_strncmp_test.c
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup_test.c                                   :+:      :+:    :+:   */
+/*   ft_strncmp_test.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akolupae <akolupae@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/24 18:59:44 by akolupae          #+#    #+#             */
-/*   Updated: 2025/04/24 20:26:52 by akolupae         ###   ########.fr       */
+/*   Created: 2025/04/24 20:35:57 by akolupae          #+#    #+#             */
+/*   Updated: 2025/04/24 20:50:30 by akolupae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
+#include <string.h>
 #include "../libft.h"
 
 void	test_input(char *input);
@@ -38,13 +38,19 @@ int	main(int argc, char **argv)
 
 void	test_input(char *input)
 {
-	char	*s_dup;
+	int			n;
+	const char	*str = "12345";
 
-	s_dup = ft_strdup(input);
-	ft_putendl_fd(input, 1);
-	ft_putendl_fd(s_dup, 1);
-	if (ft_strncmp(input, s_dup, ft_strlen(input)) == 0)
+	n = 4;
+	if (strncmp(input, str, n)  == ft_strncmp(input, str, n))
 		write(1, "PASS\n", 5);
-	free(s_dup);
+	write(1, "string 1: ", 10);
+	ft_putendl_fd((char	*) str, 1);
+	write(1, "string 2: ", 10);
+	ft_putendl_fd((char *)input, 1);
+	write(1, "strncmp: ", 9);
+	ft_putnbr_fd(strncmp(input, str, n), 1);
+	write(1, "\nMy function: ", 14);
+	ft_putnbr_fd(ft_strncmp(input, str, n), 1);
 	return ;
 }
