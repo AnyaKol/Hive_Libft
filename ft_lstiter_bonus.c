6b5235a46c4023a9f77b4662069603481220711f
Anna Kolupaeva <akolupae@c1r4p5.hive.fi>
2025-04-27 16:10:01 +0300

    Tested ft_lstnew Added libft.h to ft_lstiter

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -6,9 +6,11 @@
 /*   By: akolupae <akolupae@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 18:12:47 by akolupae          #+#    #+#             */
-/*   Updated: 2025/04/25 19:15:38 by akolupae         ###   ########.fr       */
+/*   Updated: 2025/04/27 15:00:20 by akolupae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
---
+++ tests/ft_lstnew_test.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_test.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akolupae <akolupae@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 15:02:21 by akolupae          #+#    #+#             */
+/*   Updated: 2025/04/27 16:08:02 by akolupae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+void	test_input(char *input);
+void	print_list(t_list *head);
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc == 1)
+		test_input(NULL);
+	else if (argc > 1)
+	{
+		i = 1;
+		while (i < argc)
+		{
+			test_input(argv[i]);
+			i++;
+		}
+	}
+	else
+		write(2, "Input error", 11);
+	return (0);
+}
+
+void	test_input(char *input)
+{
+	t_list	*head;
+	t_list	n1, n2, n3;
+	const char	*str = "12345";
+
+	head = &n1;
+	n1.content = (void *) &str[0];
+	n1.next = &n2;
+	n2.content = (void *) &str[1];
+	n2.next = &n3;
+	n3.content = (void *) &str[2];
+	n3.next = ft_lstnew(input);
+	print_list(head);
+	free(n3.next);
+}
+
+void	print_list(t_list *head)
+{
+	t_list	*temp;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		write(1, temp->content, 1);
+		temp = temp->next;
+	}
+}
